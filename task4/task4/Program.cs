6efc282d87044331e8d8ed2cd85f1bc700885adf
Elnur Gafarzade <138590291+1907elnur@users.noreply.github.com>
2023-07-26 13:58:58 +0400

    task6
    home work

---
+++ task4/task4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+public class User
+{
+    public string Username
+    {
+        get { return username; }
+        set
+        {
+            if (value.Length > 6)
+            {
+                username = value;
+            }
+            else
+            {
+                throw new ArgumentException("Username must be greater than 6 in length");
+            }
+        }
+    }
+
+
+    public string Password
+    {
+        get { return password; }
+        set
+        {
+            if (value.Any(char.IsDigit) && value.Any(char.IsUpper))
+            {
+                password = value;
+            }
+            else
+            {
+                throw new ArgumentException("Password must contain at least 1 number and at least 1 uppercase letter");
+            }
+        }
+    }
+
+
+    private string username;
+    private string password;
+
+
+    public User(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+}
